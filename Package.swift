8756915bdf4a3314bd8eb0c7d06677232f12e878
amir avisar <>
2024-06-18 23:19:03 +0300

    clean package.swift

--- Package.swift
+++ Package.swift
@@ -1,34 +1,14 @@
-//
-//  Package.swift
-//  glide-swift-sdk
-//
-//  Created by amir avisar on 18/06/2024.
-//
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "glide-swift-sdk",
-    platforms: [.iOS(.v17), .macOS(.v14)],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "GlideSwiftSDK",
-            targets: ["GlideSwiftSDK"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-        .package(url: "https://github.com/auth0/JWTDecode.swift", .upToNextMajor(from: "3.1.0")),
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "GlideSwiftSDK",
-            dependencies: [
-                .product(name: "Logging", package: "swift-log"),
-                .product(name: "JWTDecode", package: "JWTDecode.swift")
-            ]
-        ),
+        .executableTarget(
+            name: "glide-swift-sdk"),
     ]
 )
