1b0f2953aa02ecf9c11095fcdd33f3ace5848d8d
amir avisar <>
2024-06-18 23:37:16 +0300

    testing

--- Package.swift
+++ Package.swift
@@ -4,11 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "GlideSwiftSDK",
+    name: "glide-swift-sdk",
+        platforms: [.iOS(.v17), .macOS(.v14)],
+        products: [
+            // Products define the executables and libraries a package produces, making them visible to other packages.
+            .library(
+                name: "GlideSwiftSDK",
+                targets: ["GlideSwiftSDK"]),
+        ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "GlideSwiftSDK"),
+            name: "glide-swift-sdk"),
     ]
 )
